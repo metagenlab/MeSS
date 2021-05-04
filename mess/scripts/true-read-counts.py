7602906d6754a03859ed31e4de969cd78d8c7218
idfarbanecha <faridchaabane23@gmail.com>
2021-05-04 15:12:24 +0200

    added script for true read counts

---
+++ mess/scripts/true-read-counts.py
@@ -0,0 +1,33 @@
+from Bio import SeqIO
+import pandas as pd
+from collections import Counter
+seqtech = snakemake.params[0]
+
+
+def get_read_counts(fastq, ss):
+    """
+    function that returns a table with each genome read count
+    """
+    fastq_parser = SeqIO.parse(fastq, 'fastq')
+    headers = [record.description for record in fastq_parser]
+    if ss == 'illumina':
+        headers = [header.split('-')[-1].split('.')[0] for header in headers]
+    else:
+        headers = [header.split('.')[0] for header in headers]
+    c = dict(Counter(headers))
+    rc = pd.DataFrame.from_dict(c, orient='index', columns=['true_read_counts'])
+    rc.index.set_names('Assembly', inplace=True)
+    rc.reset_index(inplace=True)
+    return rc
+
+
+read_counts = get_read_counts(snakemake.input.reads, seqtech)
+summary = pd.read_csv(snakemake.input.table, sep='\t')
+summary['Assembly'] = [assemblyname.split('.')[0] for assemblyname in summary['AssemblyNames']]
+merged = pd.merge(summary, read_counts, on='Assembly')
+cols = ['superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'strain', 'Taxid', 'true_read_counts']
+merged[cols].to_csv(snakemake.output[0], sep='\t')
+krona_cols = ['true_read_counts', 'superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'strain']
+krona = merged[krona_cols]
+krona.set_index(columns='true_read_counts', inplace=True)
+krona.to_csv(snakemake.output.krona)
