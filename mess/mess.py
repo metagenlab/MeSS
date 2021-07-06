e20f58b115effe558f9f1e42223d6d5207de74f2
idfarbanecha <faridchaabane23@gmail.com>
2021-07-06 10:37:18 +0200

    improved code readability

--- mess/mess.py
+++ mess/mess.py
@@ -12,12 +12,14 @@ logging.basicConfig(
     format="[%(asctime)s %(levelname)s] %(message)s",
 )
 
+
 def get_snakefile():
     thisdir = os.path.abspath(os.path.dirname(__file__))
-    sf = os.path.join(thisdir,'scripts','Snakefile')
+    sf = os.path.join(thisdir, 'scripts', 'Snakefile')
     if not os.path.exists(sf):
         sys.exit(f"Unable to locate the Snakemake workflow file at {sf}")
     return sf
+
 
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.version_option(__version__)
@@ -26,19 +28,23 @@ def cli(obj):
     """
     MeSS is a snakemake workflow used for simulating metagenomic mock communities
     """
+
+
 @cli.command(
     "run",
     context_settings=dict(ignore_unknown_options=True),
     short_help="run all MeSS pipeline steps"
 )
-@click.option("-f",
+@click.option(
+    "-f",
     "--config-file",
-    type=click.Path(exists=True,resolve_path=True),
+    type=click.Path(exists=True, resolve_path=True),
     help="path to MeSS configfile",
 )
-@click.option("-p",
+@click.option(
+    "-p",
     "--conda-prefix",
-    type=click.Path(exists=True,resolve_path=True),
+    type=click.Path(exists=True, resolve_path=True),
     help="path to conda environment",
 )
 @click.option(
@@ -49,37 +55,40 @@ def cli(obj):
     show_default=True,
     help="Snakemake dryrun to see the scheduling plan",
 )
-@click.option("-nr",
+@click.option(
+    "-nr",
     "--ncbi_requests",
     type=int,
     help="number of ncbi requests, check https://github.com/metagenlab/mess for more details",
     default=3,
 )
-@click.option("-ns",
+@click.option(
+    "-ns",
     "--nb_sim",
     type=int,
     help="number of genomes to simulate in parallel, check https://github.com/metagenlab/mess for more details",
     default=2,
 )
-@click.option("-nc",
+@click.option(
+    "-nc",
     "--nb_cat",
     type=int,
     help="number of genomes to concatenate in parallel, check https://github.com/metagenlab/mess for more details",
     default=2,
 )
-@click.option("-c",
+@click.option(
+    "-c",
     "--cores",
     type=int,
     help="number of cores to allow for the workflow",
-    default=5,
+    default=10,
 )
-
 @click.argument("snakemake_args", nargs=-1, type=click.UNPROCESSED)
 def run_workflow(conda_prefix, config_file, dryrun_status, ncbi_requests, nb_sim, nb_cat, cores, snakemake_args):
     """
     Runs MeSS pipeline with all steps
     """
-    if config_file is None:#if config-file path is not set, assume that config-file is present where the command is ran
+    if config_file is None:
         config_file = 'config.yml'
 
     if not os.path.exists(config_file):
@@ -87,11 +96,11 @@ def run_workflow(conda_prefix, config_file, dryrun_status, ncbi_requests, nb_sim
                          "make sure your config.yml is in your working directory")
         sys.exit(1)
     if dryrun_status:
-        dryrun='-n'
+        dryrun = '-n'
     else:
-        dryrun=''
+        dryrun = ''
     if conda_prefix is None:
-        conda_prefix=os.environ['CONDA_PREFIX']
+        conda_prefix = os.environ['CONDA_PREFIX']
     if not os.path.exists(conda_prefix):
         logging.critical(f"conda env path not found: {config_file}\n")
         sys.exit(1)
@@ -106,5 +115,7 @@ def run_workflow(conda_prefix, config_file, dryrun_status, ncbi_requests, nb_sim
         # removes the traceback
         logging.critical(e)
         exit(1)
+
+
 if __name__ == "__main__":
     cli()
