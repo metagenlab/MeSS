59615082a76b97a6c280193aa36ccab3c51a888d
farchaab <farid.chaabane@chuv.ch>
2023-11-15 22:13:52 +0100

    added script to get replicates

---
+++ mess/bin/get_rep_table.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import random
+import pandas as pd
+import os
+import numpy as np
+from itertools import chain
+import glob
+
+
+def list_files(indir, extensions):
+    extensions = extensions.split(",")
+    files = [glob.glob(f"{indir}/*.{e}") for e in extensions]
+    return list(chain.from_iterable(files))
+
+
+random.seed(snakemake.params.seed)
+# Read input path
+if os.path.isfile(snakemake.input[0]):
+    files = snakemake.input[0]
+else:
+    files = list_files(snakemake.input[0], "tsv")
+
+# concat tables into one and add sample columns
+dfs = []
+for file in files:
+    df = pd.read_csv(file, sep="\t")
+    dfs.append(df)
+    try:
+        samples = list(set(df["sample"]))
+    except KeyError:
+        sample = os.path.basename(file).split(".")[0]
+        df["sample"] = [sample] * len(df)
+
+dfs = pd.concat(dfs).reset_index(drop=True).sort_values("sample")
+# Duplicate each sample by the number of replicates
+replicates = list(range(1, snakemake.params.rep + 1))
+rep_df = dfs.reindex(dfs.index.repeat(len(replicates)))
+# add replicate column
+rep_df["rep"] = replicates * len(dfs)
+rep_df["samplename"] = [
+    sample + "-" + str(rep) for sample, rep in zip(rep_df["sample"], rep_df["rep"])
+]
+rep_df.drop(["sample", "rep"], axis=1, inplace=True)
+accepted_cols = ["proportion", "reads", "bases", "cov_sim"]
+for col in accepted_cols:
+    try:
+        rep_df[f"{col}"] = [
+            np.random.normal(val, snakemake.params.sd_rep) for val in rep_df[f"{col}"]
+        ]
+    except KeyError:
+        continue
+
+for sample in set(rep_df["samplename"]):
+    sample_df = rep_df[rep_df["samplename"] == "sample"]
+    sample_df.to_csv(f"{snakemake.params.outdir}/{sample}.tsv", sep="\t", index=None)
