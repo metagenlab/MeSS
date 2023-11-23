4962604c3dc2429a631c6702ce64ffe49e51d15e
farchaab <farid.chaabane@chuv.ch>
2023-11-23 15:56:21 +0100

    added seed , divide total bases by sample count

--- mess/bin/calculate_cov.py
+++ mess/bin/calculate_cov.py
@@ -40,8 +40,8 @@ logging.basicConfig(
 # Set seed for distributions
 np.random.seed(snakemake.params.seed)
 
-# Set seed for seed simulation
-rng = random.Random(snakemake.params.seed)
+# Set seed for read simulators
+random.seed(snakemake.params.seed)
 
 # Pairing
 if snakemake.params.pairing:
@@ -49,48 +49,50 @@ if snakemake.params.pairing:
 else:
     p = 1
 
-# Calculate total base count
-total_bases = parse_size(snakemake.params.total_bases)
 
 # Get table with assembly genomsizes and their taxonomy
 entry_df = pd.read_csv(snakemake.input.entry, sep="\t", dtype={"entry": object})
 asm_df = pd.read_csv(snakemake.input.asm, sep="\t", dtype={"entry": object})
 df = entry_df.merge(asm_df, on="entry")
+
+# Calculate total base count
+bases_per_sample = parse_size(snakemake.params.total_bases) // df["samplename"].count()
+
 # Count number of entries
-df["count"] = df.groupby("entry")["entry"].transform("count")
+df["count"] = df.groupby(["samplename", "entry"])["entry"].transform("count")
 
 # Calculate prportion with dist
 if snakemake.params.dist == "even":
     df = get_even_dist(df, ["taxid"])
-    df["bases"] = df["proportion"] * total_bases
+    df["bases"] = df["proportion"] * bases_per_sample
     df["reads"] = df["bases"] / (snakemake.params.read_len * p)
     df["cov_sim"] = df["bases"] / df["genome_size"]
 
 elif snakemake.params.dist == "lognormal":
     df = get_lognormal_dist(df, snakemake.params.mu, snakemake.params.sigma)
-    df["bases"] = df["proportion"] * total_bases
+    df["bases"] = df["proportion"] * bases_per_sample
     df["reads"] = df["bases"] / (snakemake.params.read_len * p)
     df["cov_sim"] = df["bases"] / df["genome_size"]
 
 else:
     if "proportion" in entry_df.columns:
         df["proportion"] = df["proportion"] / df["count"]
-        df["bases"] = df["proportion"] * total_bases
+        df["bases"] = df["proportion"] * bases_per_sample
         df["reads"] = df["bases"] / (snakemake.params.read_len * p)
         df["cov_sim"] = df["bases"] / df["genome_size"]
 
     elif "reads" in entry_df.columns:
         df["reads"] = df["reads"] / df["count"]
         df["bases"] = df["reads"] * (snakemake.params.read_len * p)
-        total_bases = df["bases"].sum()
-        df["proportion"] = df["bases"] / total_bases
+        bases_per_sample = df["bases"].sum()
+        df["proportion"] = df["bases"] / bases_per_sample
         df["cov_sim"] = df["bases"] / df["genome_size"]
 
     elif "bases" in entry_df.columns:
-        total_bases = df["bases"].sum()
+        bases_per_sample = df["bases"].sum()
         df["bases"] = df["bases"] / df["count"]
         df["reads"] = df["bases"] / (snakemake.params.read_len * p)
-        df["proportion"] = df["bases"] / total_bases
+        df["proportion"] = df["bases"] / bases_per_sample
         df["cov_sim"] = df["bases"] / df["genome_size"]
 
     elif "cov_sim" in entry_df.columns:
@@ -99,9 +101,19 @@ else:
         df["reads"] = df["bases"] / (snakemake.params.read_len * p)
         df["proportion"] = df["bases"] / df["bases"].sum()
 
-df = df.astype({"bases": int, "reads": int})
+
 df["fasta"] = [os.path.basename(fa).split(".fna.gz")[0] for fa in df["path"]]
-df["seed"] = rng.sample(range(1, 1000000), len(df))
-sel_df = df[["fasta", "genome_size", "proportion", "bases", "reads", "cov_sim", "seed"]]
-sel_df.to_csv(snakemake.log[0], sep="\t", index=None)
-sel_df.set_index("fasta").to_json(snakemake.output[0], index=None)
+df["seed"] = random.sample(range(1, 1000000), len(df))
+cols = [
+    "samplename",
+    "fasta",
+    "genome_size",
+    "bases",
+    "reads",
+    "proportion",
+    "cov_sim",
+    "seed",
+]
+df = df.astype({"bases": int, "reads": int, "seed": int})
+df[cols].to_csv(snakemake.log[0], sep="\t", index=None)  # type: ignore
+df[cols].set_index(["samplename", "fasta"]).to_csv(snakemake.output[0], sep="\t")  # type: ignore
