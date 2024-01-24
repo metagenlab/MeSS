cefcd66ddf0622c26975b0dd1c1a53959c2dc03b
farchaab <farid.chaabane@chuv.ch>
2024-01-24 17:19:08 +0100

    bumped python and snakemake version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     name="mess",
     packages=find_packages(),
     url="https://github.com/metagenlab/MeSS",
-    python_requires=">=3.9",
+    python_requires=">=3.11",
     description="Snakemake pipeline for simulating short and long read metagenomes",
     long_description=get_description(),
     long_description_content_type="text/markdown",
@@ -53,7 +53,7 @@ setup(
     py_modules=["mess"],
     install_requires=[
         "snaketool-utils>=0.0.4",
-        "snakemake>=7.14.0",
+        "snakemake>=8.2.3",
         "attrmap>=0.0.5",
         "pyyaml>=6.0",
         "Click>=8.1.3",
