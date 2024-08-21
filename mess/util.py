4f79f28cb5b32f8d80afe9219c90ce65c3676e53
farchaab <farid.chaabane@chuv.ch>
2024-08-21 11:59:59 +0200

    updated run_snakemake software deployment method

--- mess/util.py
+++ mess/util.py
@@ -1,6 +1,16 @@
 import os
 import rich_click as click
-from snaketool_utils.cli_utils import echo_click
+import yaml
+import sys
+import subprocess
+from snaketool_utils.cli_utils import (
+    msg,
+    read_config,
+    update_config,
+    copy_config,
+    echo_click,
+    msg_box,
+)
 
 
 def snake_base(rel_path):
@@ -27,6 +37,118 @@ def default_to_output(ctx, param, value):
     if param.default == value:
         return os.path.join(ctx.params["output"], value)
     return value
+
+
+def get_deployment_prefix(ctx, param, value):
+    """Callback for click options; joins software deployment name and prefix dir"""
+    if not value:
+        return os.path.join(os.getcwd(), ".snakemake", ctx.params["sdm"])
+    return value
+
+
+def run_snakemake(
+    configfile=None,
+    system_config=None,
+    snakefile_path=None,
+    merge_config=None,
+    threads=1,
+    sdm=None,
+    prefix=None,
+    snake_default=None,
+    snake_args=[],
+    profile=None,
+    workflow_profile=None,
+    system_workflow_profile=None,
+    log=None,
+    **kwargs,
+):
+    """Run a Snakefile!
+
+    Args:
+        configfile (str): Filepath of config file to pass with --configfile
+        system_config (str): Filepath of system config to copy if configfile not present
+        snakefile_path (str): Filepath of Snakefile
+        merge_config (dict): Config values to merge with your config file
+        threads (int): Number of local threads to request
+        deployment (str): Snakemake's --sdm option
+        prefix (str): Filepath for Snakemake's --sdm option
+        snake_default (list): Snakemake args to pass to Snakemake
+        snake_args (list): Additional args to pass to Snakemake
+        profile (str): Name of Snakemake profile
+        workflow_profile (str): Name of Snakemake workflow-profile
+        system_workflow_profile (str): Filepath of system workflow-profile config.yaml to copy if not present
+        log (str): Log file for writing STDERR
+        **kwargs:
+
+    Returns (int): Exit code
+    """
+
+    snake_command = ["snakemake", "-s", snakefile_path]
+
+    # if using a configfile
+    if configfile:
+        # copy sys default config if not present
+        copy_config(configfile, system_config=system_config, log=log)
+
+        if merge_config:
+            update_config(in_config=configfile, merge=merge_config, log=log)
+
+        snake_command += ["--configfile", configfile]
+
+        # display the runtime configuration
+        snake_config = read_config(configfile)
+        msg_box(
+            "Runtime config",
+            errmsg=yaml.dump(snake_config, Dumper=yaml.Dumper),
+            log=log,
+        )
+
+    # add threads
+    if "--profile" not in snake_args and profile is None:
+        snake_command += ["--cores", threads]
+
+    # add software deployment args
+    if sdm:
+        if sdm == "conda":
+            snake_command += ["--sdm conda"]
+            if prefix:
+                snake_command += ["--conda-prefix", prefix]
+        if sdm == "apptainer":
+            snake_command += ["--sdm apptainer"]
+            if prefix:
+                snake_command += ["--apptainer-prefix", prefix]
+    # add snakemake default args
+    if snake_default:
+        snake_command += snake_default
+
+    # add any additional snakemake commands
+    if snake_args:
+        snake_command += list(snake_args)
+
+    # allow double-handling of --profile
+    if profile:
+        snake_command += ["--profile", profile]
+
+    # allow double-handling of --workflow-profile
+    if workflow_profile:
+        # copy system default if not present
+        copy_config(
+            os.path.join(workflow_profile, "config.yaml"),
+            system_config=system_workflow_profile,
+            log=log,
+        )
+
+        snake_command += ["--workflow-profile", workflow_profile]
+
+    # Run Snakemake!!!
+    snake_command = " ".join(str(s) for s in snake_command)
+    msg_box("Snakemake command", errmsg=snake_command, log=log)
+    if not subprocess.run(snake_command, shell=True).returncode == 0:
+        msg("ERROR: Snakemake failed", log=log)
+        sys.exit(1)
+    else:
+        msg("Snakemake finished successfully", log=log)
+    return 0
 
 
 def common_options(func):
@@ -66,15 +188,17 @@ def common_options(func):
             show_default=False,
         ),
         click.option(
-            "--use-conda/--no-use-conda",
-            default=True,
-            help="Use conda for Snakemake rules",
+            "--sdm",
+            type=click.Choice(["conda", "apptainer"]),
+            default="conda",
+            help="Software deplolyment method",
             show_default=True,
         ),
         click.option(
-            "--conda-prefix",
-            default=snake_base(os.path.join("workflow", "conda")),
-            help="Define conda env directory",
+            "--prefix",
+            default=None,
+            help="Custom softawre deployment directory",
+            callback=get_deployment_prefix,
             type=click.Path(),
             show_default=False,
         ),
