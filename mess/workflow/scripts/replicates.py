1bcef6dbf97c28cd82366e42456cc035fe92c7a2
farchaab <farid.chaabane@chuv.ch>
2024-10-04 14:12:29 +0200

    fixed replicates column length

--- mess/workflow/scripts/replicates.py
+++ mess/workflow/scripts/replicates.py
@@ -12,7 +12,7 @@ samples_df = df.reindex(df.index.repeat(len(snakemake.params.replicates)))
 if len(snakemake.params.replicates) < 2:
     samples_df.rename(columns={"sample": "samplename"}, inplace=True)
 else:
-    samples_df["rep"] = snakemake.params.replicates * len(samples_df)
+    samples_df["rep"] = snakemake.params.replicates * len(df)
     samples_df["samplename"] = [
         sample + "-" + str(rep)
         for sample, rep in zip(samples_df["sample"], samples_df["rep"])
