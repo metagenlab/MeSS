936e912b2c6f613856705fd1d68e3a660da558bd
farchaab <farid.chaabane@chuv.ch>
2025-07-14 08:55:05 +0200

    get realpath from taxonkit dir

--- mess/util.py
+++ mess/util.py
@@ -31,9 +31,7 @@ def custom_taxonkit_dir(config):
     if config["args"]["taxonkit"] == os.path.join(os.getcwd(), ".taxonkit"):
         return False
     else:
-        return os.path.dirname(
-            os.path.realpath(os.path.join(config["args"]["taxonkit"], "names.dmp"))
-        )
+        return os.path.realpath(config["args"]["taxonkit"])
 
 
 def snake_base(rel_path):
@@ -555,9 +553,9 @@ def download_options(func):
             show_default=True,
         ),
         click.option(
-            "--reference",
+            "--reference/--noref",
             type=bool,
-            help="Limit to reference and representative genomes",
+            help="Limit to reference and representative genomes or not",
             default=True,
             show_default=True,
         ),
